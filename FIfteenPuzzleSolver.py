9d3447b72489a260a7dc8da21b0762189a92f6df
FnHafiz <farhanhafiz55@gmail.com>
2022-03-29 23:45:47 +0700

    add class PQ and Node

--- FIfteenPuzzleSolver.py
+++ FIfteenPuzzleSolver.py
@@ -4,17 +4,36 @@ from heapq import heappush, heappop
 from numpy import true_divide
 
 class PriorityQueue :
+    # Konstruktor : Inisialisasi Priority Queue
     def __init__(self):
         self.heap = []
 
+    # Melakukan Push : Menambahkan elemen num ke PQ 
     def push(self, num):
-        heappush(self, num)
+        heappush(self.heap, num)
 
-    def pop(self, num):
-        heappop(self,num)
+    # Melakukan Pop : Menghapus elemen pertama dari PQ
+    def pop(self):
+        heappop(self.heap)
 
+    # Mengecek isEmpty : bernilai true jika kosong, false jika sebaliknya
     def isEmpty(self):
         if not self.heap:
             return True
         else :
             return False
+
+class Node :
+    def __init__(self, parent, matrix, empty_tile, cost, level):
+        self.parent = parent
+
+        self.matrix = matrix
+
+        self.empty_tile = empty_tile
+
+        self.cost = cost
+
+        self.level = level
+
+    def __lt__ (self, next):
+        self.cost < next.cost
