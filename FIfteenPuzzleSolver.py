dfbddb289cf26a049a838a181bee63b1ac7b445a
FnHafiz <farhanhafiz55@gmail.com>
2022-03-30 11:36:45 +0700

    BnB Done

--- FIfteenPuzzleSolver.py
+++ FIfteenPuzzleSolver.py
@@ -1,8 +1,5 @@
 import copy
 from heapq import heappush, heappop
-from itertools import count
-
-from numpy import true_divide
 
 class PriorityQueue :
     # Konstruktor : Inisialisasi Priority Queue
@@ -15,7 +12,7 @@ class PriorityQueue :
 
     # Melakukan Pop : Menghapus elemen pertama dari PQ
     def pop(self):
-        heappop(self.heap)
+        return heappop(self.heap)
 
     # Mengecek isEmpty : bernilai true jika kosong, false jika sebaliknya
     def isEmpty(self):
@@ -24,7 +21,7 @@ class PriorityQueue :
         else :
             return False
 
-class Node :
+class node :
     def __init__(self, parent, matrix, empty_tile, cost, level):
         self.parent = parent
 
@@ -37,7 +34,11 @@ class Node :
         self.level = level
 
     def __lt__ (self, next):
-        self.cost < next.cost
+        return self.cost < next.cost
+
+# bottom, left, top, right
+rows = [ 1, 0, -1, 0 ]
+cols = [ 0, -1, 0, 1 ]
 
 # Menghitung banyaknya jumlah ubin tidak kosong yang tidak terdapat pada susunan akhir
 def countCost (initial_matrix, final_matrix) -> int :
@@ -49,7 +50,7 @@ def countCost (initial_matrix, final_matrix) -> int :
 
     return count
 
-def newNode (parent, matrix, empty_tile, new_empty_tile, level, final_matrix):
+def newNode (parent, matrix, empty_tile, new_empty_tile, level, final_matrix) -> node:
     # Menyalin matrix awal ke matrix baru
     new_matrix = copy.deepcopy(matrix)
 
@@ -62,11 +63,86 @@ def newNode (parent, matrix, empty_tile, new_empty_tile, level, final_matrix):
     new_matrix[x1][y1], new_matrix[x2][y2] =  new_matrix[x2][y2], new_matrix[x1][y1]
 
     # Menghitung banyaknya jumlah ubin yang berbeda dengan susunan akhir
-    cost = countCost(new_matrix, final_matrix)
+    new_cost = countCost(new_matrix, final_matrix)
 
-    return node(parent, new_matrix, new_empty_tile, cost, level)
+    new_node = node(parent, new_matrix, new_empty_tile, new_cost, level)
+    return new_node
 
 
+# Mengecek apakah indeks x dan y valid pada matriks
+def isValid (x,y):
+    return x>=0 and x<4 and y>=0 and y<4
+
+
+# Menyelesaikan 15-Puzzle
+def Solve(initial_matrix, empty_tile, final_matrix):
+    
+    # Inisialisasi Priority Queue
+    pq = PriorityQueue()
+
+    # Menghitung banyaknya jumlah ubin yang berbeda dengan susunan akhir
+    current_cost = countCost(initial_matrix, final_matrix)
+
+    # Membuat node baru berupa root dari node awal
+    root_node = node(None, initial_matrix, empty_tile, current_cost, 0)
+
+    # Melakukan push root_node sebelumnya ke dalam Priority Queue
+    pq.push(root_node)
+
+    while not pq.isEmpty(): 
+        min_node = pq.pop()
+
+        # Jika cost = 0, artiny tidak ada lagi ubin yang berbeda dengan susunan akhir
+        if min_node.cost == 0 :
+            printPath(min_node)
+            return
+        
+        for i in range(4):
+            new_tile = [min_node.empty_tile[0] + rows[i],
+                        min_node.empty_tile[1] + cols[i],]
+            
+            if (isValid(new_tile[0], new_tile[1])):
+                child = newNode(min_node, min_node.matrix, min_node.empty_tile, new_tile, min_node.level + 1, final_matrix)
+                pq.push(child)
+                # printMatrix(min_node.matrix)
+                # printMatrix(child.matrix)
+
+
+def printMatrix(mat):
+     
+    for i in range(4):
+        for j in range(4):
+            if (mat[i][j] > 0 and mat[i][j] <= 9) :
+                print("%d " % (mat[i][j]) + " ", end = " ")   
+            elif (mat[i][j] == 16) :
+                  print("-  ", end = " ")   
+            else :       
+                print("%d " % (mat[i][j]), end = " ")     
+        print()
+
+def printPath(root_node):
+     
+    if root_node == None:
+        return
+     
+    printPath(root_node.parent)
+    printMatrix(root_node.matrix)
+    print()
+
+
+initial_matrix = [ [ 1, 2, 3, 4 ],
+                   [ 5, 6, 16, 8 ],
+                   [ 9, 10, 7, 11 ],
+                   [ 13, 14, 15, 12 ] ]
+
+final_matrix = [ [ 1, 2, 3, 4 ],
+                 [ 5, 6, 7, 8 ],
+                 [ 9, 10, 11, 12 ],
+                 [ 13, 14, 15, 16 ] ]
+
+empty_tile = [ 1, 2 ]
+
+Solve(initial_matrix, empty_tile, final_matrix)
 
 
 
